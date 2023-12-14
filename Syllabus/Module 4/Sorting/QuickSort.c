e45d59888859c68873472ec7386ee68467d7643b
ShaswataG <121812904+ShaswataG@users.noreply.github.com>
2023-12-14 21:37:20 +0530

    Quick Sort

--- Syllabus/Module 4/Sorting/QuickSort.c
+++ Syllabus/Module 4/Sorting/QuickSort.c
@@ -1,10 +1,53 @@
 #include<stdio.h>
 
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int partition(int a[], int l, int h) {
     int pivot = a[l];
     int i = l, j = h;
+    while (i < j) {
+        while (a[i] <= pivot) i++;
+        while (a[j] > pivot) j--;
+        if (i < j) swap(&a[i], &a[j]);
+    }
+    swap(&a[l], &a[j]);
+    return j;
+}
+
+void quickSort(int a[], int l, int h) {
+    int j;
+    if (l < h) {
+        j = partition(a, l, h);
+        quickSort(a, l, j-1);
+        quickSort(a, j+1, h);
+    }
+}
+
+void display(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 }
 
 int main() {
+    int size;
+    printf("Enter size : ");
+    scanf("%d", &size);
+    int a[size];
+    printf("Enter %d elements : ", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &a[i]);
+    }
+    display(a, size);
+
+    printf("%d\n", a[100]);
+    quickSort(a, 0, size-1);
+
+    display(a, size);
     return 0;
 }
