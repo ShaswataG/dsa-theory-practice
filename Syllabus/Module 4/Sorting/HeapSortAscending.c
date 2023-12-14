7822481e96c40d1a592b58b654b32c868c719bed
ShaswataG <121812904+ShaswataG@users.noreply.github.com>
2023-12-14 21:55:25 +0530

    Heap Sort  (Ascending)

---
+++ Syllabus/Module 4/Sorting/HeapSortAscending.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int insertHeap(int a[], int n) {
+//  creating a max heap
+    int i = n;
+    int temp = a[n];
+    while (i > 1 && temp > a[i/2]) {
+        a[i] = a[i/2];
+        i /= 2;
+    }
+    a[i] = temp;
+}
+
+int deleteHeap(int a[], int n) {
+    int val = a[1];
+    a[1] = a[n];
+    a[n] = val;
+    int i = 1, j = 2*i;
+    while (j < n-1) {
+        if(a[j+1] > a[j]) j++;
+        // }
+        if (a[i] < a[j]) {
+            swap(&a[i], &a[j]);
+            i = j;
+            j = 2*i;
+        } else {
+            break;
+        }
+    }
+    return val;
+}
+
+void heapSort(int a[], int n) {
+    for (int i = 2; i < n; i++) {
+        insertHeap(a, i);
+    }
+    for (int i = n-1; i > 1; i--) {
+        deleteHeap(a, i);
+    }
+}
+
+void display(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+    printf("Enter size : ");
+    scanf("%d", &size);
+    printf("Enter %d elements : ", size);
+
+    // int a[] = {0, 3, 6, 8, 10, 11, 14};
+    
+    int a[size+1];
+    a[0] = 0;
+    for (int i = 1; i <= size; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Before sorting, elements are : ");
+    display(a, size+1);
+    heapSort(a, size+1);
+
+    // for (int i = 2; i <= 6; i++) {
+    //     insertHeap(a, i);
+    // }
+    // for (int i = 6; i > 1; i--) {
+    //     deleteHeap(a, i);
+    // }
+    printf("After sorting, elements are : ");
+    display(a, size+1);
+    return 0;
+}
---
+++ Syllabus/Module 4/Sorting/HeapSortDescending.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void display(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void insertHeap(int a[], int n) {
+//  creating a min heap
+    int i = n;
+    int temp = a[n];
+    while (i > 1 && temp < a[i/2]) {
+        a[i] = a[i/2];
+        i /= 2;
+    }
+    a[i] = temp;
+}
+
+void deleteHeap(int a[], int n) {
+    int val = a[1];
+    a[1] = a[n];
+    a[n] = val;
+    int i = 1, j = 2*i;
+    while (j < n-1) {
+        if (a[j+1] < a[j]) j++;
+        if (a[i] > a[j]) {
+            swap(&a[i], &a[j]);
+            i = j;
+            j = 2*i;
+        } else {
+            break;
+        }
+    }
+}
+
+void heapSort(int a[], int n) {
+    for (int i = 2; i < n; i++) {
+        insertHeap(a, i);
+    }
+    printf("Test heap : ");
+    display(a, n);
+    for (int i = n-1; i > 1; i--) {
+        deleteHeap(a, i);
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter size : ");
+    scanf("%d", &size);
+    int a[size+1];
+    a[0] = 0;
+    printf("Enter %d elements : ", size);
+    for (int i = 1; i <= size; i++) {
+        scanf("%d", &a[i]);
+    }
+    display(a, size+1);
+    heapSort(a, size+1);
+    display(a, size+1);
+    return 0;
+}
